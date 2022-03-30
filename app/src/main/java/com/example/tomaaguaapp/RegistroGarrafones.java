ceb5801156e79cc3568f3923f6f66c4216b38d7c
pedrofdez23 <pedrofdezcolli@gmail.com>
2022-03-30 17:12:31 -0600

    addRegister
    dadsf

---
+++ app/src/main/java/com/example/tomaaguaapp/RegistroGarrafones.java
@@ -0,0 +1,14 @@
+package com.example.tomaaguaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroGarrafones extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_garrafones);
+    }
+}
