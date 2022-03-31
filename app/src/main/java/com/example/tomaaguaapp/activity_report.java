6d764a65ee9cebb13fe7ec310576a89fe7831d99
luis <lichos_99@hotmail.com>
2022-03-31 15:48:33 -0600

    cambios luis
    subiendo mis modificaciones

---
+++ app/src/main/java/com/example/tomaaguaapp/activity_report.java
@@ -0,0 +1,41 @@
+package com.example.tomaaguaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class activity_report extends AppCompatActivity {
+    TextView tv;
+    Spinner opciones;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+        opciones = (Spinner) findViewById(R.id.edificios);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Opciones, android.R.layout.simple_spinner_item);
+        opciones.setAdapter(adapter);
+    }
+    public void abrircalendario(View view) {
+        Calendar cal = Calendar.getInstance();
+        int anio = cal.get(Calendar.YEAR);
+        int mes = cal.get(Calendar.MONTH);
+        int dia = cal.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dpd = new DatePickerDialog(activity_report.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                String fecha = dayOfMonth + "/" + month + "/" + year;
+                tv.setText(fecha);
+            }
+        },dia, mes, anio);
+        dpd.show();
+    }
+}
