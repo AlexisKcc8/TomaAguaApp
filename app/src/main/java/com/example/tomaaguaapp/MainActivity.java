da68150db4f6902106f2de7fc88aeb6d683f3c1b
pedrofdez23 <pedrofdezcolli@gmail.com>
2022-03-31 08:52:25 -0600

    Registrof
    si

--- app/src/main/java/com/example/tomaaguaapp/MainActivity.java
+++ app/src/main/java/com/example/tomaaguaapp/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.tomaaguaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
