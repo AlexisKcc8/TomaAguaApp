c9ab9af8b4e7d1764caf0fd0cee627bae6b4712d
Alejandro <nohbryan10@gmail.com>
2022-04-01 11:24:22 -0600

    Arreglos
    Nuevos Activities

--- app/src/main/java/com/example/tomaaguaapp/RegistroGarrafones.java
+++ app/src/main/java/com/example/tomaaguaapp/RegistroGarrafones.java
@@ -8,10 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class RegistroGarrafones extends AppCompatActivity {
-    Button btn_report;
+    Button btn_go_reportes;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_garrafones);
+        btn_go_reportes = findViewById(R.id.button_verreportes);
+
+        btn_go_reportes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegistroGarrafones.this, show_report_activity.class);
+                startActivity(i);
+            }
+        });
     }
 }
