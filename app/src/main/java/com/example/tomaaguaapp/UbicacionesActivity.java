48d7ff083aafde5abffd91a7e8b77fea57aa446a
Alexis Chim <chimalx732@gmail.com>
2022-04-01 14:27:35 -0600

    edit_manifest_status

--- app/src/main/java/com/example/tomaaguaapp/UbicacionesActivity.java
+++ app/src/main/java/com/example/tomaaguaapp/UbicacionesActivity.java
@@ -2,13 +2,17 @@ package com.example.tomaaguaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class UbicacionesActivity extends AppCompatActivity {
 
     Spinner spListaUbicaciones;
+    Button btn_showStatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,15 @@ public class UbicacionesActivity extends AppCompatActivity {
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Opciones, android.R.layout.simple_spinner_item);
         spListaUbicaciones.setAdapter(adapter);
+
+        btn_showStatus = findViewById(R.id.btn_status);
+        btn_showStatus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(UbicacionesActivity.this, garrafon_status.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
